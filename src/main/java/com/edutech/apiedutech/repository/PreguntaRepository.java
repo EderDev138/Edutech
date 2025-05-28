7f1dfb6763532229a0cce122f7af94a766dc76a8
dayana-vega <da.vegab@duocuc.cl>
2025-05-27 23:12:50 -0400

    se crea PreguntaRepositorio

---
+++ src/main/java/com/edutech/apiedutech/repository/PreguntaRepository.java
@@ -0,0 +1,9 @@
+package com.edutech.apiedutech.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edutech.apiedutech.model.Pregunta;
+
+public interface PreguntaRepository extends JpaRepository <Pregunta, Integer> {
+
+}
