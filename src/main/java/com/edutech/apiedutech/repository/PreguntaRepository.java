52f1f15004de6946379cc183deb8f2aec0834bca
dayana-vega <da.vegab@duocuc.cl>
2025-05-27 23:15:32 -0400

    lista repositorio pregunta

--- src/main/java/com/edutech/apiedutech/repository/PreguntaRepository.java
+++ src/main/java/com/edutech/apiedutech/repository/PreguntaRepository.java
@@ -3,7 +3,9 @@ package com.edutech.apiedutech.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edutech.apiedutech.model.Pregunta;
+import java.util.List;
+
 
 public interface PreguntaRepository extends JpaRepository <Pregunta, Integer> {
-
+    List<Pregunta> findById(int usuarioId);
 }
---
+++ src/main/java/com/edutech/apiedutech/repository/RespuestaRepository.java
@@ -0,0 +1,9 @@
+package com.edutech.apiedutech.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edutech.apiedutech.model.Respuesta;
+
+public interface RespuestaRepository extends JpaRepository <Respuesta, Integer>{
+
+}
