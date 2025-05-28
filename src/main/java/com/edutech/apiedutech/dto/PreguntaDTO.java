8cf3ab6feaad3b66c89743213add5ca1bc55d043
dayana-vega <da.vegab@duocuc.cl>
2025-05-27 23:35:08 -0400

    se crea pregunta dto

---
+++ src/main/java/com/edutech/apiedutech/dto/PreguntaDTO.java
@@ -0,0 +1,27 @@
+package com.edutech.apiedutech.dto;
+
+public class PreguntaDTO {
+     private int id;
+     private String enunciado;
+     private int evlauacionId;
+     public int getId() {
+         return id;
+     }
+     public void setId(int id) {
+         this.id = id;
+     }
+     public String getEnunciado() {
+         return enunciado;
+     }
+     public void setEnunciado(String enunciado) {
+         this.enunciado = enunciado;
+     }
+     public int getEvlauacionId() {
+         return evlauacionId;
+     }
+     public void setEvlauacionId(int evlauacionId) {
+         this.evlauacionId = evlauacionId;
+     }
+
+     
+}
