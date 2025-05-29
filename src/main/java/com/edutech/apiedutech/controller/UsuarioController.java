63159f6a5c46e3fa6c4a2597259a69ce971e30cd
EderDev138 <e.valdivia.aguirre@gmail.com>
2025-05-29 17:11:18 -0400

    se crea asignacion

--- src/main/java/com/edutech/apiedutech/controller/UsuarioController.java
+++ src/main/java/com/edutech/apiedutech/controller/UsuarioController.java
@@ -3,12 +3,14 @@ package com.edutech.apiedutech.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.edutech.apiedutech.dto.AsignacionUsuarioDTO;
 import com.edutech.apiedutech.dto.UsuarioDTO;
 import com.edutech.apiedutech.model.Usuario;
 import com.edutech.apiedutech.service.UsuarioService;
@@ -44,4 +46,14 @@ public class UsuarioController {
             return dto;
         }).toList();
     }
+
+    @DeleteMapping("/eliminar/{rut}")
+    public String eliminarUsuario(String rut) {
+        return usuarioService.eliminarUsuario(rut);
+    }
+    // se asigna por body
+   @PostMapping("/asignarcurso")
+    public String asignarCurso(@RequestBody AsignacionUsuarioDTO dto) {
+    return usuarioService.inscripcion(dto.getRut(), dto.getSigla());
+}
 }
---
+++ src/main/java/com/edutech/apiedutech/dto/AsignacionUsuarioDTO.java
@@ -0,0 +1,28 @@
+package com.edutech.apiedutech.dto;
+
+public class AsignacionUsuarioDTO {
+    private String rut;
+    private String sigla;
+
+
+
+    public String getRut() {
+        return rut;
+    }
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+    public String getSigla() {
+        return sigla;
+    }
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+
+
+    
+}
+
+
+
