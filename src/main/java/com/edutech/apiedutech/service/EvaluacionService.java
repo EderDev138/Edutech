61bf1f14ee88d529f9c557391da86997e1b0afa7
dayana-vega <da.vegab@duocuc.cl>
2025-05-28 20:52:46 -0400

    actualizacion

--- src/main/java/com/edutech/apiedutech/service/EvaluacionService.java
+++ src/main/java/com/edutech/apiedutech/service/EvaluacionService.java
@@ -1,20 +1,15 @@
 package com.edutech.apiedutech.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.edutech.apiedutech.model.Evaluacion;
 import com.edutech.apiedutech.repository.EvaluacionRepository;
 
 public class EvaluacionService {
 
     @Autowired
     private EvaluacionRepository evaluacionRepository;
+    
 
-    public List<Evaluacion> listar(){
-        return evaluacionRepository.findAll();
 
-    }
 
 }
