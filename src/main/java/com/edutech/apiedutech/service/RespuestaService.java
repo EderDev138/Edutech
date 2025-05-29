cf8fc13e35afd30c2ae122b45eed525ee8f4466e
enportillo <en.portillo@duocuc.cl>
2025-05-29 12:58:43 -0400

    se eliminan dependencias no utilizadas

--- src/main/java/com/edutech/apiedutech/service/RespuestaService.java
+++ src/main/java/com/edutech/apiedutech/service/RespuestaService.java
@@ -5,17 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.edutech.apiedutech.model.Evaluacion;
 import com.edutech.apiedutech.model.Pregunta;
-import com.edutech.apiedutech.model.Respuesta;
 import com.edutech.apiedutech.repository.PreguntaRepository;
-import com.edutech.apiedutech.repository.RespuestaRepository;
 
 @Service
 public class RespuestaService {
-
-    @Autowired
-    private RespuestaRepository respuestaRepository;
 
     @Autowired
     private PreguntaRepository preguntaRepository;
