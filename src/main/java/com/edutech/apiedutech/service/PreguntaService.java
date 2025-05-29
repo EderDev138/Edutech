df8ab5f683ffe02515523654b3cc0ff2673cb9bb
dayana-vega <da.vegab@duocuc.cl>
2025-05-28 21:46:15 -0400

    se actualiza PreguntaService

--- src/main/java/com/edutech/apiedutech/service/PreguntaService.java
+++ src/main/java/com/edutech/apiedutech/service/PreguntaService.java
@@ -1,5 +1,21 @@
 package com.edutech.apiedutech.service;
 
-public interface PreguntaService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.edutech.apiedutech.model.Pregunta;
+import com.edutech.apiedutech.repository.PreguntaRepository;
+
+public class PreguntaService {
+
+    @Autowired
+    private PreguntaRepository preguntaRepository;
+
+    public List <Pregunta> listar(){
+        return preguntaRepository.findAll();
+
+    }
+
+    
 }
