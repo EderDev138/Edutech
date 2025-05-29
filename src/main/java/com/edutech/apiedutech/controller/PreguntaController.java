53dadc160b19781271c3c5afbdae078b98f8b63b
dayana-vega <da.vegav@duocuc.cl>
2025-05-29 19:12:21 -0400

    Se ajusta Deletemapping a pregunta

--- src/main/java/com/edutech/apiedutech/controller/PreguntaController.java
+++ src/main/java/com/edutech/apiedutech/controller/PreguntaController.java
@@ -1,6 +1,7 @@
 package com.edutech.apiedutech.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +27,7 @@ public class PreguntaController {
 
     }
     
-    @PostMapping("/eliminar/{id}")
+    @DeleteMapping("/eliminar/{id}")
     public String eliminarPregunta(@PathVariable int id) {
         return preguntaService.eliminarPregunta(id);
     }
