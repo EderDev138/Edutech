b6aa2417824696a7b97558dcaeb6693a407bf447
dayana-vega <da.vegav@duocuc.cl>
2025-06-14 14:16:25 -0400

    se crea repositorio PAGO

---
+++ src/main/java/com/edutech/apiedutech/repository/PagoRepository.java
@@ -0,0 +1,11 @@
+package com.edutech.apiedutech.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edutech.apiedutech.model.Pago;
+
+// REALIZADO POR: DAYANA VEGA
+public interface PagoRepository extends JpaRepository <Pago, Integer>{
+
+
+}
