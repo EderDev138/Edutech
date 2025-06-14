1abfad5e8833bb46607ec659fc6c12ceacaf5c4f
dayana-vega <da.vegav@duocuc.cl>
2025-06-14 14:03:51 -0400

    Llenado de clase PAGO

--- src/main/java/com/edutech/apiedutech/model/Pago.java
+++ src/main/java/com/edutech/apiedutech/model/Pago.java
@@ -1,10 +1,15 @@
 package com.edutech.apiedutech.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 
 /** 
 @author "Dayana Vega"
@@ -16,7 +21,73 @@ public class Pago {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private int monto;
+    private int montoDescuento;
+    private LocalDateTime fechaPago;
 
+    @ManyToOne
+    @JoinColumn(name = "tarjeta_id", nullable = false)
+    private Tarjeta tarjeta;
 
+    @OneToOne
+    @JoinColumn(name = "cupon_id")
+    private Cupon cupon;
 
+    public Pago(){
+        this.id= 0;
+        this.monto= 0;
+        this.montoDescuento= 0;
+        this.fechaPago= LocalDateTime.now();
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public int getMontoDescuento() {
+        return montoDescuento;
+    }
+
+    public void setMontoDescuento(int montoDescuento) {
+        this.montoDescuento = montoDescuento;
+    }
+
+    public LocalDateTime getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(LocalDateTime fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public Tarjeta getTarjeta() {
+        return tarjeta;
+    }
+
+    public void setTarjeta(Tarjeta tarjeta) {
+        this.tarjeta = tarjeta;
+    }
+
+    public Cupon getCupon() {
+        return cupon;
+    }
+
+    public void setCupon(Cupon cupon) {
+        this.cupon = cupon;
+    }
+    
+    
 }
