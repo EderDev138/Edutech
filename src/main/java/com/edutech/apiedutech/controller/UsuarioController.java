8f6b0d43e6cd3512b0fb7f1c03f54902f036578d
EderDev138 <e.valdivia.aguirre@gmail.com>
2025-07-12 09:20:02 -0400

    seagrega pathvariable

--- src/main/java/com/edutech/apiedutech/controller/UsuarioController.java
+++ src/main/java/com/edutech/apiedutech/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,7 +46,7 @@ public class UsuarioController {
     }
 
     @DeleteMapping("/eliminar/{rut}")
-    public String eliminarUsuario(String rut) {
+    public String eliminarUsuario(@PathVariable String rut) {
         return usuarioService.eliminarUsuario(rut);
     }
     // se asigna por body
